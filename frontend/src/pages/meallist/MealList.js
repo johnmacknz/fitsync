65839be150c2272f1e133108847e8b9bd81b5961
maxfawcett01 <maxfawcett01@gmail.com>
2024-02-09 09:44:24 +0000

    Loading sign

--- frontend/src/pages/meallist/MealList.js
+++ frontend/src/pages/meallist/MealList.js
@@ -2,33 +2,46 @@ import React, { useState, useEffect } from 'react';
 import './MealList.css';
 import UserNavbar from "../../components/usernavbar/UserNavbar";
 import MealTab from "../../containers/mealTab/MealTab";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MealList = () => {
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(true); // State to track loading status
 
     useEffect(() => {
         const fetchMeals = () => {
             fetch('http://localhost:8080/meals')
                 .then(response => response.json())
-                .then(data => setMeals(data))
-                .catch(error => console.error('Error fetching meals:', error));
+                .then(data => {
+                    setMeals(data);
+                    setLoading(false); // Set loading to false once meals are fetched
+                })
+                .catch(error => {
+                    console.error('Error fetching meals:', error);
+                    setLoading(false); // Set loading to false in case of error
+                });
         };
 
         fetchMeals();
     }, []);
 
     return (
-                <div className="ft__login__container">
-                    <UserNavbar />
-                    <div className="tabs-container">
-                        {meals.map((meal) => (
-                            <Link key={meal.id} to={`/meals/${meal.id}`}>
-                                <MealTab meal={meal} />
-                            </Link>
-                        ))}
+        <div className="ft__login__container">
+            <UserNavbar />
+            <div className="tabs-container">
+                {loading ? (
+                    <div className="loading">
+                        Loading...
                     </div>
-                </div>
+                ) : (
+                    meals.map((meal) => (
+                        <Link key={meal.id} to={`/meals/${meal.id}`}>
+                            <MealTab meal={meal} />
+                        </Link>
+                    ))
+                )}
+            </div>
+        </div>
     );
 };
 
